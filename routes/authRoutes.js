ce852f5ea843b35860e25a607faa42321f7c3cc5
mehedihasan770 <mehedihasantrbd@gmail.com>
2026-02-23 17:22:29 +0600

    change bcrypt password hash

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -17,10 +17,13 @@ router.post('/signup', async (req, res) => {
 
         const existingUser = await usersCollection.findOne({ email });
         if(existingUser){
-            return res.status(400).json({ success: false, message: "User already exists." });
+            return res.status(400).json({ 
+                success: false, 
+                message: "User already exists." 
+            });
         }
 
-        const salt = await bcrypt.genSalt(15);
+        const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
 
         const newUser = {
@@ -56,6 +59,66 @@ router.post('/signup', async (req, res) => {
                 id: result.insertedId,
                 name: newUser.name,
                 email: newUser.email
+            }
+        });
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ success: false, message: "Server Error!" });
+
+    }
+})
+
+router.post('/signin', async (req, res) => {
+    try {
+        const { email, password, } = req.body;
+        const db = getDb();
+        const usersCollection = db.collection("users");
+
+        if (!email || !password) {
+            return res.status(400).json({ message: "Please provide email, and password." });
+        }
+
+        const user = await usersCollection.findOne({ email });
+        if(!user){
+            return res.status(400).json({ 
+                success: false, 
+                message: "Invalid credentials. User not found!" 
+            });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ 
+                success: false, 
+                message: "Invalid credentials. Password didn't match!" 
+            });
+        }
+
+        await usersCollection.updateOne(
+            { _id: user._id },
+            { $set: { lastLogin: new Date() } }
+        );
+
+        const token = jwt.sign(
+            {
+                id: user._id,
+                role: user.role
+            },
+            process.env.JWT_SECRET || 'Secret123',
+            { 
+                expiresIn: '7d'
+            }
+        );
+
+        res.status(200).json({
+            success: true,
+            message: "Login successful!",
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email
             }
         });
 
